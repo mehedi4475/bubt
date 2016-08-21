d7807101d1d6462750d1054e5ea72f165e63fe73
Mehedi Hasan <mehedi4475@gmail.com>
2016-08-20 17:40:14 -0700

    First Commit

---
+++ app/Http/routes.php
@@ -0,0 +1,85 @@
+<?php
+
+
+//login, logout and dashboard
+Route::get('/','Starlab@login');
+Route::get('/login','Starlab@login');
+Route::post('/login','Starlab@loginVerification');
+Route::get('/dashboard','Starlab@dashboard');
+
+
+
+//See All patients
+Route::get('/patients','Starlab@allPatient');
+Route::get('/patients/{id}','Starlab@allPatient');
+
+
+Route::get('/logout','Starlab@logout');
+
+
+//Search Patient
+Route::get('/search-patient','Starlab@searchPatient');
+Route::post('/search-patient','Starlab@searchPatientInfo');
+
+
+//Add patients
+Route::get('/create','Starlab@create');
+Route::post('/create','Starlab@store');
+
+//Show and delete patient
+Route::get('/patient/{id}','Starlab@showPatient');
+Route::get('/patient/{id}/delete', 'Starlab@destroy');
+
+
+//Approve patients
+Route::get('/patient/{id}/approve', 'Starlab@approve');
+
+//Request to Administrator for approve
+Route::get('/{id}/{userType}/request/{orderType}', 'Starlab@approveOrder');
+
+//Update basic information
+Route::post('/update/{id}','Starlab@update');
+Route::get('patient/{id}/edit', 'Starlab@edit');
+
+
+
+//Update medical-examinations information
+Route::post('/update-medical-examinations/{id}','Starlab@updateMedicalExaminations');
+Route::get('/{id}/edit-medical-examinations', 'Starlab@editMedicalExaminations');
+
+
+//Update laboratory investigations information
+Route::post('/update-laboratory-investigations/{id}','Starlab@updateLaboratoryInvestigations');
+Route::get('/{id}/edit-laboratory-investigations', 'Starlab@editLaboratoryInvestigations');
+
+
+
+//Update laboratory investigations information
+Route::post('/update-radiology/{id}','Starlab@updateRadiology');
+Route::get('/{id}/edit-radiology', 'Starlab@editRadiology');
+
+
+
+
+//Print
+Route::get('/{id}/{back}/print','Starlab@printPatient');
+
+
+//User
+Route::get('/user/{id}/', 'Starlab@user');
+
+
+//Fit/Unfit for search a approve patient and print front and back page
+Route::get('/fitUnfit', 'Starlab@fitUnfit');
+Route::post('/fitUnfit', 'Starlab@fitUnfitinfo');
+
+
+//test
+Route::get('/test', 'Starlab@test');
+
+
+
+Route::get('/miru', 'Starlab@miru');
+
+
+    
